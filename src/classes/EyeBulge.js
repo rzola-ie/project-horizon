d20490dea37fc65f67b1fa8edc733dc13eb01927
rzola-ie <ryan.zola@idea-evolver.com>
2022-03-31 08:34:05 -0400

    big cleanup

--- src/classes/EyeBulge.js
+++ src/classes/EyeBulge.js
@@ -135,55 +135,6 @@ export default class EyeBulge {
   }
 
   buildMaskMaterial(videoTransformMat2) {
-    /*
-      THIS IS WHERE THE DEFORMATIONS ARE BUILT:
-      1) create a tearpoint where the deformation will be located
-      2) add a displacement(x, y) to deform the zone around your tearpoint
-      3) select a radius: the bigger the radius the bigger the size of the deformed zone
-      around your tearpoint will be
-    */
-    // const vertexShaderSource = 'uniform mat2 videoTransformMat2;\n\
-    // varying vec2 vUVvideo;\n\
-    // // deformation 0 parameters:\n\
-    // const vec2 TEARPOINT0 = vec2(0.,-0.5);\n\
-    // const vec2 DISPLACEMENT0 = vec2(0.,0.15);\n\
-    // const float RADIUS0 = 0.4;\n\
-    // // deformation 1 parameters:\n\
-    // const vec2 TEARPOINT1 = vec2(0.25,-0.4);\n\
-    // const vec2 DISPLACEMENT1 = vec2(0.12,-0.07);\n\
-    // const float RADIUS1 = 0.3;\n\
-    // // deformation 2 parameters:\n\
-    // const vec2 TEARPOINT2 = vec2(-0.25,-0.4);\n\
-    // const vec2 DISPLACEMENT2 = vec2(-0.12,-0.07);\n\
-    // const float RADIUS2 = 0.3;\n\
-    // void main() {\n\
-    //   vec3 positionDeformed=position;\n\
-    //   // apply deformation 0\n\
-    //   float deformFactor0 = 1.-smoothstep(0.0, RADIUS0, distance(TEARPOINT0, position.xy));\n\
-    //   positionDeformed.xy += deformFactor0*DISPLACEMENT0;\n\
-    //   // apply deformation 1\n\
-    //   float deformFactor1 = 1.-smoothstep(0.0, RADIUS1, distance(TEARPOINT1, position.xy));\n\
-    //   positionDeformed.xy += deformFactor1*DISPLACEMENT1;\n\
-    //   // apply deformation 2\n\
-    //   float deformFactor2 = 1. - smoothstep(0.0, RADIUS2, distance(TEARPOINT2, position.xy));\n\
-    //   positionDeformed.xy += deformFactor2*DISPLACEMENT2;\n\
-    //   // project deformed point:\n\
-    //   vec4 mvPosition = modelViewMatrix * vec4( positionDeformed, 1.0 );\n\
-    //   vec4 projectedPosition=projectionMatrix * mvPosition;\n\
-    //   gl_Position=projectedPosition;\n\
-    //   // compute UV coordinates on the video texture:\n\
-    //   vec4 mvPosition0 = modelViewMatrix * vec4( position, 1.0 );\n\
-    //   vec4 projectedPosition0 = projectionMatrix * mvPosition0;\n\
-    //   vUVvideo = vec2(0.5,0.5) + videoTransformMat2 * projectedPosition0.xy / projectedPosition0.w;\n\
-    // }';
-
-    // const fragmentShaderSource = "precision mediump float;\n\
-    // uniform sampler2D samplerVideo;\n\
-    // varying vec2 vUVvideo;\n\
-    // void main() {\n\
-    //   gl_FragColor = texture2D(samplerVideo, vUVvideo);\n\
-    // }";
-
     const mat = new THREE.ShaderMaterial({
       vertexShader: vertex,
       fragmentShader: fragment,
