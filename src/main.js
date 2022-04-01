e170b26132c7fe2770923ddebf136bf2fb27ca74
rzola-ie <ryan.zola@idea-evolver.com>
2022-04-01 11:31:00 -0400

    remove vue-head

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import VueHead from 'vue-head'
 
 createApp(App)
-    .use(VueHead)
     .use(router)
     .mount('#app')
