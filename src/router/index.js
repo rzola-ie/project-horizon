d8d3a2b5f0a5dea722d8efd2175952edd6fa9ba8
rzola-ie <ryan.zola@idea-evolver.com>
2022-05-04 14:10:00 -0400

    remove embed stuff

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Home from '../components/Home.vue'
 import LiveMode from '../components/LiveMode.vue'
 import Experience from '@/components/Experience.vue'
-import Embedded from '@/components/Embedded.vue'
 
 const routes = [
   {
@@ -14,11 +12,6 @@ const routes = [
     path: '/experience',
     name: 'liveExperience',
     component: Experience,
-  },
-  {
-    path: '/embed',
-    name: 'embed',
-    component: Embedded,
   },
 ]
 
